ca57aeaa12a58a8d86fddba987602429cd8bc75b
Remi Appels <mail@remii.nl>
2011-09-26 15:09:33 +0200

    Creating gameLoop

--- src/nl/vaya/mgdd/rjp/GameEngine.java
+++ src/nl/vaya/mgdd/rjp/GameEngine.java
@@ -1,9 +1,14 @@
 package nl.vaya.mgdd.rjp;
 
 import android.graphics.Canvas;
+import android.util.Log;
 
 public class GameEngine {
 
-	void Update(){}
+	static final String logTag = "log_tag";
+	
+	void Update(){
+		Log.i(logTag, "Update");
+	}
 	void Draw(Canvas c){}
 }
