42166be4770344b8b3ed52ec27d0bfe5183cb9bd
Jasper Kennis <jasper@creativetoken.nl>
2011-10-03 15:19:31 +0200

    Forced own communicator to front.

--- src/nl/vaya/mgdd/rjp/connection/Communicator.java
+++ src/nl/vaya/mgdd/rjp/connection/Communicator.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.io.Reader;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -50,10 +51,6 @@ public class Communicator implements MessageResponder {
 	
 	public void recieveMessages(MessageResponder callback) {
 		String nextLine = null;
-
-		while ((nextLine = receiver.readLine()) != null) {
-			  callback.respond( nextLine);
-			}
 		try {
 			while ((nextLine = receiver.readLine()) != null) {
 				if( receiver.ready() ) {
